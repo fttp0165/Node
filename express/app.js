c679b085bdd3d9542a274797a343c44f2317359c
Benny <benny08118@gmail.com>
2020-01-07 02:02:16 +0800

    add POST function

--- express/app.js
+++ express/app.js
@@ -1,35 +1,62 @@
 var express=require('express');
 var app=express();
+var engine=require('ejs-locals');
+app.engine('ejs',engine);
 //console.log(app);
+var bodyParser=require('body-parser');
 
-
+//增加靜態檔案路徑
 app.use(express.static('public'));
+
+//增加body 解析
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(
+{extended:false}))
+
+
 
 app.use(function(req,res,next){
     console.log('有人登入');
     next();
 })
-var engine=require('ejs-locals');
-app.engine('ejs',engine);
+
+
+app.get('/',function(req,res){
+    // res.send('index.html');
+     res.send('進入首頁');
+})
+
+app.get('/search',function(req,res){
+   res.render('search');
+})
+app.post('/searchList',function(req,res){
+    console.log(req.body);
+    //顯示searchText 的值
+    console.log(req.body.searchText);
+})
+
 //設定樣板在views 資料夾
 app.set('views','./views');
 //設定使用樣板
 app.set('view engine','ejs');
 
 
-app.get('/',function(req,res){
-    // res.send('index.html');
-    // res.send(/index.html);
-    res.render('index',{'show':false,
-    'title':'<h1>Benny</h1>',
-     'BOSS':'Benny',
-     'course':['lin','KK','YOYO']});
-})
-app.get('/user',function(req,res){
-    // res.send('index.html');
-    // res.send(/index.html);
-    res.render('user');
-})
+// app.get('/',function(req,res){
+//     // res.send('index.html');
+//     // res.send(/index.html);
+//     res.render('index',{'show':false,
+//     'title':'<h1>Benny</h1>',
+//      'BOSS':'Benny',
+//      'course':['lin','KK','YOYO']});
+// })
+
+
+
+// app.get('/user',function(req,res){
+//     // res.send('index.html');
+//     // res.send(/index.html);
+//     res.render('user');
+// })
 
 //監聽port
 var port=process.env.PORT||3000;
